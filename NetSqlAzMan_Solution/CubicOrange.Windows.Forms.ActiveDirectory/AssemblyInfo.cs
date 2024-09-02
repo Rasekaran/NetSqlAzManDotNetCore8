7260cc2694c446b5ea7137bd000db9bf53a4de3c
Atputharajah Rasekaran <atputharajah.rasekaran@q4us.dev>
2024-09-02 11:59:42 +0530

    Convert all other convertable projects to .Net core 8.(Some unable to convert)

--- NetSqlAzMan_Solution/CubicOrange.Windows.Forms.ActiveDirectory/AssemblyInfo.cs
+++ NetSqlAzMan_Solution/CubicOrange.Windows.Forms.ActiveDirectory/AssemblyInfo.cs
@@ -1,32 +1,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-//
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-//
-[assembly: AssemblyTitle("Directory Object Picker common dialog")]
-[assembly: AssemblyDescription(".NET SQL Authorization Manager")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Directory Object Picker")]
-[assembly: AssemblyCopyright("© 2004 Armand du Plessis <armand@dotnet.org.za>")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("2.0.0.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
